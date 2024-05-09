c5db1137b130fb121c0ba0fd39e9209305c44637
GAMASTI\leonardo.miranda <leonardo.miranda@moovefy.com.br>
2024-05-09 11:49:51 -0300

    first commit

---
+++ cypress/e2e/Evernot_TrabalhoAutomação/teste1.cy.js
@@ -0,0 +1,18 @@
+describe('Primeiro Teste: Fazendo login no site Evernote', () => {
+    
+    before(() => {
+
+        //Fazendo login no site
+        cy.visit('https://evernote.com/pt-br')
+
+
+    })
+    it('mapeando', () =>{
+
+        cy.get('.relative > .bg-accent').click()
+        cy.get('.rich-wrapper--hero-vertical > :nth-child(4) > a').click()
+        cy.get('#email').type('leomiranda844@gmail.com')
+        cy.get('form > .flex-col > .flex').click()
+
+    })
+})
